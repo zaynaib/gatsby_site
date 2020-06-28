32994430b9736c6f2a11f53c82c5c1dbc1360aad
Zaynaib Giwa <zaynaibg@gmail.com>
2020-06-28 00:56:48 -0500

    add website layout to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 export default function Contact() {
   return (
+    <Layout>
     <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
       <Header headerText="Contact" />
       <p>Send us a message!</p>
     </div>
+    
+    </Layout>
   )
 }
