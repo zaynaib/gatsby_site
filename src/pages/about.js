8ae06541e8df3fa2c30aaf43dbfd39b381bad43c
Zaynaib Giwa <zaynaibg@gmail.com>
2020-07-09 00:35:54 -0500

    add projects to portfolio

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,8 +26,8 @@ export default function About({ data }) {
           <ul>
             <li>Co-organizer of R-Ladies Chicago - An organization that promotes gender diversity in the R community</li>
             <li>Teaching Assistant for Hacking 4 Justice - An organization that provides free R training to anyone.</li>
-            <li>        <Link to="/posts/">Blogging</Link> about things that I learn.</li> 
-            <li>Hanging out with CoolCats <img src="https://i.pinimg.com/736x/1c/6a/a4/1c6aa47e0e518b552e7be7ea08f761ed.jpg"/> - A meetup group that helps people transition from coding bootcamp into their tech careers </li>
+            <li> <Link to="/posts/">Blogging</Link> about things that I learn.</li> 
+            <li>Hanging out with CoolCats <img src="https://i.pinimg.com/736x/1c/6a/a4/1c6aa47e0e518b552e7be7ea08f761ed.jpg"/>  A meetup group that helps people transition from coding bootcamp into their tech careers </li>
           </ul>        
         </p>
       </AboutLayout>
