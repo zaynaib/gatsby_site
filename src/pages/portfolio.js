af4982d0d20f8701fdc979290e7484ce0a682280
Zaynaib Giwa <zaynaibg@gmail.com>
2020-07-14 19:54:00 -0500

    conditional rendering for portfolio page if website it there

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -8,9 +8,9 @@ export default function Portfolio() {
     <Layout>
 
     
-        <PortfolioLayout name = "Frogger Arcade Game"  description="Copy Cat of the 1980's popular Game Frogger" webpage=" https://zaynaib.github.io/frogger-arcade-game/" github= "https://github.com/zaynaib/frogger-arcade-game/"/>
-        <PortfolioLayout name = "Memory Card Game"  description=" A card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn.  See how many matches you can get before you run out of chances  😝 ." webpage="https://zaynaib.github.io/fend-project-memory-game/" github="https://github.com/zaynaib/fend-project-memory-game"/>
-        <PortfolioLayout name = "Museum Map of Chicago"  description="An interactive map" webpage="https://wicked-labyrinth-31457.herokuapp.com/" github="https://github.com/zaynaib/map"/>
+        <PortfolioLayout name = "Frogger Arcade Game"  description="Copy Cat of the 1980's popular Game Frogger" webpage=" https://zaynaib.github.io/frogger-arcade-game/" github= "https://github.com/zaynaib/frogger-arcade-game/" isWebPage={true}/>
+        <PortfolioLayout name = "Memory Card Game"  description=" A card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn.  See how many matches you can get before you run out of chances  😝 ." webpage="https://zaynaib.github.io/fend-project-memory-game/" github="https://github.com/zaynaib/fend-project-memory-game" isWebPage={true}/>
+        <PortfolioLayout name = "Museum Map of Chicago"  description="An interactive map" webpage="https://wicked-labyrinth-31457.herokuapp.com/" github="https://github.com/zaynaib/map" isWebPage={true}/>
         <PortfolioLayout name = "Chicago Python (ChiPy) Mentorship"  description="A collection of Flask Apps that I created during my mentorship with the ChiPy community" github="https://github.com/zaynaib/chipy_mentorship"/>
         <PortfolioLayout name = "Young Democrats"  description="Data munging exercise to filter House Representives younger than 45 who have social media accounts using Python's Pandas Library" github="https://www.kaggle.com/amazingspecialist/datamade-test"/>
 
