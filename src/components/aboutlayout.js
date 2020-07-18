075524d36d2156a5f92614b1c7a867424eaca050
Zaynaib Giwa <zaynaibg@gmail.com>
2020-07-18 11:59:34 -0500

    add about profile image

--- src/components/aboutlayout.js
+++ src/components/aboutlayout.js
@@ -1,12 +1,13 @@
 import React from "react"
 import "./about.css"
+import PowerPuff from "../images/powerpuff.png"
 //http://www.jasminewilson.com/about - inspo
 
 export default function AboutLayout(props) {
   return (<div className="container">
     
             <div className="photo">
-                <img src="https://controllergear.com/media/catalog/product/cache/1/image/040ec09b1e35df139433887a97daa66f/c/o/controller-gear-pokemon-phone-tech-badge-pikachu-face_1.jpg" />
+                <img src={PowerPuff}/>
             </div>
 
             <div className="about-story">
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ export default function Home() {
   return (
     <Layout>
 
-      <Talk month="October" year= "2017" link="http://chicago.act-w.org/" name="ACT-W Chicktech Chicago" description= "Is FreeCodeCamp a Good Way to Learn How to Code? - Intro to Data Analysis with R"/>
-      <Talk month="June" year= "2018" link="https://www.chicagocodecamp.com/" name="Chicago Code Camp" description= "D3.js for beginners "/>
-      <Talk month="October" year= "2018" link="https://www.meetup.com/People-of-Color-Code-and-Coffee/events/255596194/" name="People of Color Code" description= "People of Color Code - Intro to Git and Github "/>
-      <Talk month="November" year= "2018" link="https://www.meetup.com/rladies-chicago/events/255727048/" name="R-Ladies Chicago" description= "'Git' down with Git + R (Workshop & Tutorial)"/>
+      <Talk month="May" year= "2020" link="https://chicago2020.satrdays.org/" name="Using Data Science For Social Good" description= "Using Data Science For Social Good"/>      
       <Talk month="December" year= "2018" link="https://www.meetup.com/WriteSpeakCode-Chicago/events/256192666/" name="Write/Speak/Code" description= "Mentor for Own Your Expertise Events"/>
-      <Talk month="May" year= "2020" link="https://chicago2020.satrdays.org/" name="Using Data Science For Social Good" description= "Using Data Science For Social Good"/>
-      
+      <Talk month="November" year= "2018" link="https://www.meetup.com/rladies-chicago/events/255727048/" name="R-Ladies Chicago" description= "'Git' down with Git + R (Workshop & Tutorial)"/>
+      <Talk month="October" year= "2018" link="https://www.meetup.com/People-of-Color-Code-and-Coffee/events/255596194/" name="People of Color Code" description= "People of Color Code - Intro to Git and Github "/>
+      <Talk month="June" year= "2018" link="https://www.chicagocodecamp.com/" name="Chicago Code Camp" description= "D3.js for beginners "/>
+      <Talk month="October" year= "2017" link="http://chicago.act-w.org/" name="ACT-W Chicktech Chicago" description= "Is FreeCodeCamp a Good Way to Learn How to Code? - Intro to Data Analysis with R"/>
+
     </Layout>
   )
 }
