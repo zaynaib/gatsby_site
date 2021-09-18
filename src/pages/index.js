db10d5ed971ecb9b220a67b832548390e4fb4f05
Zaynaib Giwa <zaynaibg@gmail.com>
2021-09-17 23:18:16 -0500

    updated talks

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,10 @@ import Talk from "../components/talks"
 export default function Home() {
   return (
     <Layout>
-      <Talk month="April" year= "2021" link="https://www.meetup.com/Cleveland-UseR-Group/events/276353088/" name="R-Users Cleveland" description= "D3 for R Users"/>      
+
+      <Talk month="April" year= "2021" link="https://www.meetup.com/Cleveland-UseR-Group/events/276353088/" name="R-Users Cleveland" description= "D3 for R Users"/>    
+      <Talk month="March" year= "2021" link="https://www.coursera.org/projects/d3-basics" name="Coursera" description= "D3JS Basics"/>      
+      <Talk month="March" year= "2021" link="https://www.coursera.org/projects/creating-personal-site-gatsby" name="Coursera" description= "Creating a Personal Site with Gatsby"/>        
       <Talk month="May" year= "2020" link="https://chicago2020.satrdays.org/" name="satRday Chicago 2020" description= "Using Data Science For Social Good"/>      
       <Talk month="December" year= "2018" link="https://www.meetup.com/WriteSpeakCode-Chicago/events/256192666/" name="Write/Speak/Code" description= "Mentor for Own Your Expertise Events"/>
       <Talk month="November" year= "2018" link="https://www.meetup.com/rladies-chicago/events/255727048/" name="R-Ladies Chicago" description= "'Git' down with Git + R (Workshop & Tutorial)"/>
