664a65ae66818a53ba73d1b8eff3a4e3501ba4b4
Zaynaib Giwa <zaynaibg@gmail.com>
2021-09-18 15:41:06 -0500

    change font family with bulma

--- src/components/newVizLayout.js
+++ src/components/newVizLayout.js
@@ -6,26 +6,14 @@ export default function Vizlayout(props){
     return(
         
               
-          <div className={cardStyles.container}>
-
-            <div className={cardStyles.container}>
-              <img src={props.img} class="img-fluid" />
-            </div>
-
-
-            <div className="card-body">
-
-              <h5 class="card-title">{props.title}</h5>
-              <p class="card-text">
-                {props.desc} <br></br>
-                <a href={props.link} class="btn btn-primary">Check it out</a>
-
-              </p>
-
-              
-            </div>
-          </div>
-        
+      <div className="card">
+      <img className="card-img-top" src="..." alt="Card image cap"></img>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
        
 
     )
