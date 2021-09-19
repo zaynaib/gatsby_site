02cb80b162a05ba677453b2f424c4ece097da0ba
Zaynaib Giwa <zaynaibg@gmail.com>
2021-09-19 10:02:36 -0500

    playing around with header footer layout templates

---
+++ src/components/layout-original.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import layoutStyles from "../styles/layout.module.css"
+import favicon from '../images/favicon.ico'
+import ListLink from "./listLink"
+
+
+export default function Layout({children }) {
+  return (
+    <div className={layoutStyles.layout} >
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Code with Ola</title>
+      <link rel="icon" href={favicon}/>
+    </Helmet>
+    <nav>
+      <ul class="navbar-menu" style={{ listStyle: `none`, float: `right` }}>
+     
+      <ListLink  to="/">Home</ListLink>
+        <ListLink to="/talks/">Talks</ListLink>
+        <ListLink to="/posts/">Blog</ListLink>
+        <ListLink to="/portfolio/">Portfolio</ListLink>
+        <ListLink to="/dataviz/">Data Viz</ListLink>
+
+      </ul>
+      </nav>
+    
+    <header style={{ marginBottom: `1.5rem` }}>
+     <h1 className="title">Hi I'm Ola</h1>
+     <h2 className="title"> Data Analyst/ Full-Stack Enthusiast</h2>
+    </header>
+
+
+    <div className="content">
+
+      {children}
+      </div>
+
+      <footer class="is-flex is-justify-content-center">
+        <a href="https://github.com/zaynaib">Github</a>
+        <a href="https://twitter.com/AmazingSpeciali">Twitter</a>
+        <a href="https://www.linkedin.com/in/zaynaib-ola-giwa/">LinkedIn</a>
+        <a href="https://www.youtube.com/channel/UCCKgha-Y5wmJ2abaG9EusMA">Youtube</a>
+      </footer>
+    </div>
+  )
+}
